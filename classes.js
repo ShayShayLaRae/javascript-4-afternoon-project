62d13411114b73572be0bd415c22acb93493a646
Shayla Paulsen <shayla.paulsen@gmail.com>
2019-10-11 14:38:07 -0600

    first commit

--- classes.js
+++ classes.js
@@ -30,7 +30,18 @@
 */
 
 //Code Here
-
+class Employee {
+  constructor(first_name, last_name, email, age){
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.age = age
+    
+  }
+  makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`
+  }
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -48,7 +59,27 @@
 */
 
 //Code Here
-
+class Manager {
+  constructor(first_name, last_name, email, age, reports){
+    this.first_name = first_name
+    this.last_name = last_name
+    this.email = email
+    this.age = age
+    this.reports = reports
+  }
+  makeWidget(){
+    return`${this.first_name} ${last_name} Widget`
+  }
+  report(){
+    return `${this.first_name} ${this.last_name} ${this.email} ${this.age}`
+  }
+  hire(employee){
+    return 
+  }
+  fire(index){
+    return
+  }
+}
 
 ////////// PROBLEM 3 //////////
 
